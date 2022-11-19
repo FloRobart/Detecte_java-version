b5e9e54fa438957b17f331b999c73c5d52594638
Floris <Floris.robart11@gmail.com>
2022-11-20 00:27:29 +0100

    correction des derniers bug après amélioration

--- VerifJavaVersion.bat
+++ VerifJavaVersion.bat
@@ -1,11 +1,10 @@
 @echo off
 
-:: Permet de savoir si java existe, si ce n'est pas le cas ouvre une alerte puis télécharge java jdk 16 ::
-java -versio || (
+:: Permet de savoir si java existe, si ce n'est pas le cas ouvre une boite de dialogue puis télécharge java jdk 16 ::
+java -version || (
     call :javaInvalide "Vous n'avez pas java, Voulez-vous télécharger Java jdk 16 ?"
     goto :eof
 )
-
 
 :: Vérifie la version de java ::
 FOR /F "usebackq delims=" %%A IN (`java -version 2^>^&1`) DO echo %%A | findstr /i "16 17 18 19" && (
@@ -28,7 +27,7 @@ goto :eof
 :: Si java n'est pas valide, lance la boite de dialogue pour télécharge java jdk 16 ::
 :javaInvalide
     :: Ecrit le script qui vas generer la boite de dialogue pour installer java ::
-    echo result = MsgBox("Vous n'avez pas java, Voulez-vous télécharger Java jdk 16 ?", vbYesNo+vbQuestion) > javaInvalide.vbs
+    echo result = MsgBox("%~1%", vbYesNo+vbQuestion) > javaInvalide.vbs
     echo If result = vbYes Then >> javaInvalide.vbs
     echo     WScript.CreateObject ("Wscript.shell").run("installationJava16.bat"), 0 >> javaInvalide.vbs
     echo End If >> javaInvalide.vbs
